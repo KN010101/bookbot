a25b4ba96f9f6204adbf3eb3c8cd20d00d30f3fa
KN010101 <9662960+KN010101@users.noreply.github.com>
2025-10-24 22:08:00 -0400

    Count Characters in Text

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
 def main():
-    book_path="/home/kadmin/workspace/github.com/KN010101/bookbot/books/frankenstein.txt"
+    book_path = "/home/kadmin/workspace/github.com/KN010101/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    char_count = get_char_count(text)
     print(f"{num_words} words found in the document")
+    for char, count in char_count.items():
+        print(f"'{char}': {count}")
 
 
 def get_book_text(path):
+    """
+    Reads the content of a text file.
+
+    Parameters:
+        path (str): The path to the text file.
+
+    Returns:
+        str: The content of the file as a string.
+    """
     with open(path) as f:
         return f.read()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,21 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_char_count(text, lowercase=True):
+    """
+    Counts the occurrences of each character in the given text.
+
+    Args:
+        text (str): The input text to analyze.
+        lowercase (bool, optional): If True, converts text to lowercase before counting. Defaults to True.
+
+    Returns:
+        dict: A dictionary where keys are characters and values are their counts in the text.
+    """
+    if lowercase:
+        text = text.lower()
+    chars = {}
+    for char in text:
+        chars[char] = chars.get(char, 0) + 1
+    return chars
